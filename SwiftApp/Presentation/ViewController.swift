04453d0c5cc08c97605fd260a205bca319c3a766
Harshini Lakshmi <harshini@a2ml15936m.local>
2018-07-30 16:52:10 +0530

    Moving collection view layout delegate calls to another file

--- SwiftApp/Presentation/ViewController.swift
+++ SwiftApp/Presentation/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Alamofire //library to make api call
 import SwiftyJSON //to parse the json response
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, CustomCollectionViewLayoutDelegate {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     //**************************************************
     // MARK: - Enum Constants
@@ -27,16 +27,6 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     let apiUrl = URL(string: Constants.apiUrl)
     var parsedJSONArray = [AnyObject]()
     var responseDictionary: NSDictionary?
-    
-    var items: [AnyObject] {
-        var items = [AnyObject]()
-        if let responseDict = responseDictionary,
-            let responseItems = responseDict["rows"] as? [AnyObject] {
-            items = responseItems as [AnyObject]
-            return items
-        }
-        return items
-    }
     
     //**************************************************
     // MARK: - IBOutlets
@@ -91,65 +81,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         self.refreshControl.endRefreshing()
     }
     
-    // MARK: UICollectionView delegate methods
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.parsedJSONArray.count
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCollectionViewCell.Constants.reuseIdentifier, for: indexPath) as? CustomCollectionViewCell {
-            cell.configureCell(withData: items[indexPath.row] as! ResponseData)
-            self.mainCollectionView.collectionViewLayout.prepare()
-            return cell
-        }
-        return UICollectionViewCell()
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func collectionView(collectionView: UICollectionView, heightForImageAt indexPath: IndexPath, with width: CGFloat) -> CGFloat
-    {
-        return 200.0
-    }
-    
-    func collectionView(collectionView: UICollectionView, heightForTitleAt indexPath: IndexPath, with width: CGFloat) -> CGFloat
-    {
-        if let currentData = items[indexPath.row] as? ResponseData {
-            let topPadding = CGFloat(4)
-            let bottomPadding = CGFloat(4)
-            let captionFont = UIFont.systemFont(ofSize: 15)
-            let captionHeight = self.height(for: currentData.title!, with: captionFont, width: width)
-            let height = topPadding + captionHeight + bottomPadding
-            return height
-        }
-        return 0.0
-    }
-    
-    func collectionView(collectionView: UICollectionView, heightForDescriptionAt indexPath: IndexPath, with width: CGFloat) -> CGFloat
-    {
-        if let currentData = items[indexPath.row] as? ResponseData {
-            let topPadding = CGFloat(4)
-            let bottomPadding = CGFloat(4)
-            let captionFont = UIFont.systemFont(ofSize: 15)
-            let captionHeight = self.height(for: currentData.description!, with: captionFont, width: width)
-            let height = topPadding + captionHeight + bottomPadding
-            
-            return height
-        }
-        return 0.0
-    }
-    
-    func height(for text: String, with font: UIFont, width: CGFloat) -> CGFloat
-    {
-        let nsstring = NSString(string: text)
-        let maxHeight = CGFloat(300.0)
-        let textAttributes = [NSAttributedStringKey.font : font]
-        let boundingRect = nsstring.boundingRect(with: CGSize(width: width, height: maxHeight), options: .usesLineFragmentOrigin, attributes: textAttributes, context: nil)
-        return ceil(boundingRect.height)
     }
 }
